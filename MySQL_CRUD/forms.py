d9e9c342d66d5a58f001ae421678f56f6aaecc69
Balaji G <balajig21031998@gmail.com>
2024-11-10 12:55:38 +0530

    Delete MySQL_CRUD directory

--- MySQL_CRUD/forms.py
+++
@@ -1,7 +0,0 @@
-from django import forms
-from .models import RegisterFrom
-
-class MyRegisterForm(forms.ModelForm):
-    class Meta:
-        model=RegisterFrom
-        fields=["name","age","address","contact","email"]
